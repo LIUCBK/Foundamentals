48130bb011a42397e6d38e4fc68dbbec48513074
LIUCBK <liucbk@bc.edu>
2025-02-02 12:42:42 -0500

    Initial Commit

---
+++ Foundamentals/FoundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundamentalsApp.swift
+//  Foundamentals
+//
+//  Created by 刘佳雨 on 2025/2/2.
+//
+
+import SwiftUI
+
+@main
+struct FoundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
